fbef6b0c6bfc8a7f1274be1d6394f4bb6632e09b
Théo <theo.combelles@gmail.com>
2021-06-13 07:22:31 +0900

    final_theo

--- essai/test_catch_objet.cpp
+++ essai/test_catch_objet.cpp
@@ -8,25 +8,25 @@
 #include <cmath>
 #include <SFML/Graphics.hpp>
 
-#include "bonus.hpp"
+#include "objet.hpp"
 
 
 
 
-TEST_CASE( "bonus can be created", "[bonus]" ) {
+TEST_CASE( "objet can be created", "[objet]" ) {
 
     // Pour chaque SECTION, cet initialisation est faite - voir https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md#test-cases-and-sections
     // Et pour plus d'infos : https://github.com/catchorg/Catch2/blob/master/docs/test-cases-and-sections.md#type-parametrised-test-cases
     
-    Bonus bonus(0, 1);
+    Objet objet("name", "bonus.png",0, 1);
     
 
     // Tests constructeur
-    REQUIRE(bonus.getX() == 0);
-    REQUIRE(bonus.getY() == 1);
-    REQUIRE(bonus.getW() == 10);
-    REQUIRE(bonus.getH() == 10);
-    REQUIRE(bonus.getScore() == 10);
-    REQUIRE(bonus.getName() == "bonus");
+    REQUIRE(objet.getX() == 0);
+    REQUIRE(objet.getY() == 1);
+    REQUIRE(objet.getW() == 10);
+    REQUIRE(objet.getH() == 10);
+    REQUIRE(objet.getScore() == 10);
+    REQUIRE(objet.getName() == "name");
 
 }
